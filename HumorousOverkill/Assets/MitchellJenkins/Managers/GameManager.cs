b16c596cf19766021c60505599db45a945d44699
Mitch <mjmt997@gmail.com>
2017-10-23 10:38:21 +1100

    Managers

---
+++ HumorousOverkill/Assets/MitchellJenkins/Managers/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+struct PlayerInfo {
+    int m_playerHealth;
+    float m_playerSpeed;
+    float m_playerJumpHeight;
+    int m_pickupHealthAmount;
+    int m_pickupAmmoAmount;
+    float m_gunFireRate_type1;
+    float m_gunFireRate_type2;
+    float m_gunDamage_type1;
+    float m_gunDamage_type2;
+    int m_gunMaxAmmo;
+}
+
+public class GameManager : MonoBehaviour {
+    [SerializeField] PlayerInfo plyinfo;
+}
