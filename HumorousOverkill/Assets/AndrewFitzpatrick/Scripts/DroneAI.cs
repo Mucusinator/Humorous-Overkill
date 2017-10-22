c6cc5dddd42a7786e7cd572de1af080db1f4eef5
Ian <iankruz98@gmail.com>
2017-10-23 10:25:40 +1100

    Im in the wrong neighbourhood

--- HumorousOverkill/Assets/AndrewFitzpatrick/Scripts/DroneAI.cs
+++ HumorousOverkill/Assets/AndrewFitzpatrick/Scripts/DroneAI.cs
@@ -1,43 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
-public class DroneAI : MonoBehaviour
-{
-    // wandering
-    [Header("Wander Behavior")]
-    public float wanderRadius;
-    public float wanderJitter;
-    public float wanderDistance;
-    private Vector3 previousTarget;
+public class DroneAI : MonoBehaviour {
 
-	void Update ()
-    {
-        // target random point on wander radius
-        Vector3 target = getRandomVector(wanderRadius);
-
-        // add jitter and renormalize to wander radius
-        target += getRandomVector(wanderJitter);
-        target.Normalize();
-        target *= wanderRadius;
-
-        target += transform.forward.normalized * wanderDistance;
-
-        transform.Translate(target * Time.deltaTime);
+	// Use this for initialization
+	void Start () {
+		
 	}
-
-    Vector3 getRandomVector(float radius)
-    {
-        Vector2 vec2D = Random.insideUnitCircle.normalized * radius;
-        return new Vector3(vec2D.x, 0, vec2D.y);
-    }
-
-    void OnDrawGizmos()
-    {
-        UnityEditor.Handles.color = Color.red;
-        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.up, wanderRadius);
-        UnityEditor.Handles.color = Color.yellow;
-        UnityEditor.Handles.DrawWireDisc(transform.position + Vector3.forward * wanderRadius, Vector3.up, wanderJitter);
-    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
