8c8d88407ea94bf3cc5964ec2c1146a9cf24f027
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-10-23 14:17:31 +1100

    Better Drone AI

--- HumorousOverkill/Assets/AndrewFitzpatrick/Scripts/DroneAI.cs
+++ HumorousOverkill/Assets/AndrewFitzpatrick/Scripts/DroneAI.cs
@@ -11,10 +11,15 @@ public class DroneAI : MonoBehaviour
     public float errorMargin;
     public float wanderSpeed;
     public float turnSpeed;
+    public float avoidRadius;
     public bool showGizmos = false;
     private Vector3 currentTarget;
-    [SerializeField]
-    private float turnTime = 0.0f;
+    private RaycastHit hitInfo;
+
+    void Start()
+    {
+        pickTarget();
+    }
 
 	void Update ()
     {
@@ -23,18 +28,22 @@ public class DroneAI : MonoBehaviour
 
     void wander()
     {
-        // look at target and move forward
-        Vector3 direction = (currentTarget - transform.position).normalized;
-        Quaternion toRotation = Quaternion.FromToRotation(transform.forward, direction);
+        Vector3 direction = currentTarget - transform.position;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
 
-        turnTime = Mathf.Min(1.0f, turnTime + (1.0f / turnSpeed * Time.deltaTime));
+        //transform.LookAt(currentTarget);
+        transform.Translate(transform.forward * wanderSpeed * Time.deltaTime, Space.World);
 
-
-        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, turnTime);
-        transform.Translate(transform.forward * wanderSpeed * Time.deltaTime);
+        if(Physics.Raycast(transform.position, transform.forward * avoidRadius, out hitInfo))
+        {
+            if(hitInfo.collider.gameObject.tag == "Avoid")
+            {
+                pickTarget();
+            }
+        }
 
         // if we are within the margin of error pick a new target
-        if((transform.position - currentTarget).sqrMagnitude < Mathf.Pow(errorMargin, 2))
+        if ((transform.position - currentTarget).sqrMagnitude < Mathf.Pow(errorMargin, 2))
         {
             pickTarget();
         }
@@ -45,8 +54,6 @@ public class DroneAI : MonoBehaviour
         currentTarget = transform.position + getRandomVector(targetRadius);
 
         // TODO: avoid walls etc
-
-        turnTime = 0.0f;
     }
 
     Vector3 getRandomVector(float radius)
@@ -55,7 +62,7 @@ public class DroneAI : MonoBehaviour
         return new Vector3(vec2D.x, 0, vec2D.y);
     }
 
-    void OnDrawGizmos()
+    void OnDrawGizmosSelected()
     {
         if(showGizmos)
         {
@@ -63,10 +70,16 @@ public class DroneAI : MonoBehaviour
             UnityEditor.Handles.color = Color.red;
             UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.up, targetRadius);
 
-            // show currentVelocity
-            UnityEditor.Handles.color = Color.green;
-            UnityEditor.Handles.DrawLine(transform.position, currentTarget);
+            // display margin of error
+            UnityEditor.Handles.color = Color.yellow;
+            UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.up, errorMargin);
 
+            // show currentVelocity
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, transform.position + transform.forward * wanderSpeed);
+
+            // display current target
+            Gizmos.color = Color.green;
             Gizmos.DrawWireSphere(currentTarget, 0.5f);
         }
     }
