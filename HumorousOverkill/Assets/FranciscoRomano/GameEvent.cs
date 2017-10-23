e0e3613abfb17594a33d9399af6e26df405a65a2
Francisco Romano <blastsilver@gmail.com>
2017-10-23 11:19:19 +1100

    jhasgdgasdjkgdsa

--- HumorousOverkill/Assets/FranciscoRomano/GameEvent.cs
+++ HumorousOverkill/Assets/FranciscoRomano/GameEvent.cs
@@ -1,7 +1,11 @@
 ﻿public enum GameEvent
 {
     // Game Enum
-
+    GAME_STATE_MENU,
+    GAME_STATE_START,
+    GAME_STATE_PAUSE,
+    GAME_STATE_RESTART,
+    GAME_STATE_CONTINUE,
     // Enemy Enum
 
     // Player Enum
---
+++ HumorousOverkill/Assets/FranciscoRomano/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : GameEventListener
+{
+    public void Test()
+    {
+
+    }
+
+    public void HandleTriggerEvent(GameEvent e)
+    {
+        HandleEvent(e);
+    }
+
+    override public void HandleEvent(GameEvent e)
+    {
+        switch (e)
+        {
+            // handle game states
+            case GameEvent.GAME_STATE_MENU:
+                break;
+            case GameEvent.GAME_STATE_START:
+                break;
+            case GameEvent.GAME_STATE_PAUSE:
+                break;
+            case GameEvent.GAME_STATE_RESTART:
+                break;
+            case GameEvent.GAME_STATE_CONTINUE:
+                break;
+            // default handle
+            default:
+                base.HandleEvent(e);
+                break;
+        }
+    }
+}
