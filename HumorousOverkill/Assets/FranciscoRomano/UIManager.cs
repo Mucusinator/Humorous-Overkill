7fbe7a821ad460b8ce99fbd07cf1133e2a79c58c
Francisco Romano <blastsilver@gmail.com>
2017-10-23 12:22:25 +1100

    changed pickup

--- HumorousOverkill/Assets/FranciscoRomano/UIManager.cs
+++ HumorousOverkill/Assets/FranciscoRomano/UIManager.cs
@@ -1,20 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : GameEventListener
 {
-    public void Test()
-    {
+    public GameObject m_playerStatsHealth = null;
+    public GameObject m_playerStatsMaxAmmo = null;
+    public GameObject m_playerStatsCurrentAmmo = null;
 
-    }
-
-    public void HandleTriggerEvent(GameEvent e)
-    {
-        HandleEvent(e);
-    }
-
-    override public void HandleEvent(GameEvent e)
+    public override void HandleEvent(GameEvent e)
     {
         switch (e)
         {
@@ -28,6 +23,33 @@ public class UIManager : GameEventListener
             case GameEvent.GAME_STATE_RESTART:
                 break;
             case GameEvent.GAME_STATE_CONTINUE:
+                break;
+            // default handle
+            default:
+                base.HandleEvent(e);
+                break;
+        }
+    }
+
+    public override void HandleEvent(GameEvent e, float amount)
+    {
+        switch (e)
+        {
+            // handle game states
+            case GameEvent.GAME_STATE_MENU:
+
+                break;
+            case GameEvent.GAME_STATE_START:
+
+                break;
+            case GameEvent.GAME_STATE_PAUSE:
+
+                break;
+            case GameEvent.GAME_STATE_RESTART:
+
+                break;
+            case GameEvent.GAME_STATE_CONTINUE:
+
                 break;
             // default handle
             default:
