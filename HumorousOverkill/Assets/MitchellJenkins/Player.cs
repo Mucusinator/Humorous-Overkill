b64b9db2b10e3b33fe33472f4be8e13a249682d9
Mitch <mjmt997@gmail.com>
2017-10-23 14:17:38 +1100

    Manager V_4

--- HumorousOverkill/Assets/MitchellJenkins/Player.cs
+++ HumorousOverkill/Assets/MitchellJenkins/Player.cs
@@ -6,12 +6,26 @@ public class Player : MonoBehaviour {
     public bool isHealthFull{
         get { return m_plyInfo.m_playerHealth == 100 ? true : false; }
     }
+    public bool isDead {
+        get { return m_plyInfo.m_playerHealth == 0 ? true : false; }
+    }
+
 
     public void AddHealth (int health) {
         m_plyInfo.m_playerHealth += health;
+        CheckHealth();
+    }
+
+    public void TakeDamage(int damage) {
+        m_plyInfo.m_playerHealth -= damage;
+        CheckHealth();
+    }
+
+
+    void CheckHealth () {
         if (m_plyInfo.m_playerHealth > 100)
             m_plyInfo.m_playerHealth = 100;
         else if (m_plyInfo.m_playerHealth < 0)
-            m_plyInfo.m_playerHealth = 100;
+            m_plyInfo.m_playerHealth = 0;
     }
 }
