8c748e8ed5bed15dd4db5f00b1f4d0cde94394b6
Zackary D <zackary.direen@hotmail.com>
2017-10-23 11:08:49 +1100

    assesrts + basic script

---
+++ HumorousOverkill/Assets/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public float gunDamage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+   
+
+	
+
+	void Update () {
+        //If the user presses the left mouse button, perform the shoot function.
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+
+    void Shoot()
+    {
+        // A variable that will store the imformation gathered from the raycast.
+        RaycastHit hit;
+
+        // If we hit something with our shot raycast.
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) ;
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+
+}
+
