aa20be5aee3b96ae80be9a486d015dca29de7598
Mitch <mjmt997@gmail.com>
2017-10-23 12:28:02 +1100

    Manager V1_2

---
+++ HumorousOverkill/Assets/MitchellJenkins/Managers/EnemyManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+// Struct holding all the Game info
+[System.Serializable] struct EnemyInfo {
+    public int m_enemyHealth_type1;
+    public int m_enemyHealth_type2;
+    public float m_enemySpeed_type1;
+    public float m_enemySpeed_type2;
+    public float m_enemyDamage_type1;
+    public float m_enemyDamage_type2;
+    public int m_enemyAmmoDropRate;
+}
+
+public class EnemyManager : GameEventListener {
+
+    [SerializeField] EnemyInfo m_enemyInfo;
+
+    public override void HandleEvent (GameEvent e) {
+
+    }
+
+}
