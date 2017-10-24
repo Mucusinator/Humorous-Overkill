ba67d7729c03353d1fc256c080e425c9b1999a1f
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-10-24 15:44:11 +1100

    Fixed all issues since problem

--- HumorousOverkill/Assets/AndrewFitzpatrick/Scripts/DonutAI.cs
+++ HumorousOverkill/Assets/AndrewFitzpatrick/Scripts/DonutAI.cs
@@ -16,9 +16,9 @@ public class DonutAI : MonoBehaviour
     private Transform modelTransform;
 
     // debug
-    public Vector3 target;
+    public GameObject target;
 
-    void Start ()
+    void Start()
     {
         // get values from manager
         // health
@@ -30,29 +30,43 @@ public class DonutAI : MonoBehaviour
         // deployTime
         findCircumference();
         modelTransform = GetComponentsInChildren<Transform>()[1];
-	}
+    }
 
-    void Update ()
+    void Update()
     {
         roll();
-	}
+    }
 
     void roll()
     {
-        Vector3 direction = (target - transform.position).normalized;
-        modelTransform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
+        // find direction to target
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        direction.y = 0;
 
+        // rotate parent to look at target
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
+
+        // A wheel moves forward a distance equal to its circumference with each rotation.
         modelTransform.Rotate(new Vector3(0, rollSpeed * 360 / donutCircumference, 0) * Time.deltaTime);
-        transform.Translate(direction * rollSpeed * Time.deltaTime, Space.World);
+
+        // roll forward
+        transform.Translate(transform.forward * rollSpeed * Time.deltaTime, Space.World);
     }
 
+    // sets donutCircumference
     void findCircumference()
     {
+        // access the boxCollider of the mesh
         BoxCollider donutCollider = GetComponentInChildren<BoxCollider>();
 
+        // get the "x" size of the collider (actually y)
         float size = donutCollider.size.x;
+
+        // debug the diameter of the mesh
         Debug.Log("the diameter of the donut is " + size);
 
+        // circumference is 2PIr aka PI * diameter
+        // also takes into account scaling
         donutCircumference = (size * Mathf.PI * transform.localScale.y);
     }
 }
