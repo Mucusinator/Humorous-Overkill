0c2b867f66a49683fc3cea6aef699603eea80a31
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-10-24 16:24:11 +1100

    Implementing donut Attack AI

--- HumorousOverkill/Assets/AndrewFitzpatrick/Scripts/DonutAI.cs
+++ HumorousOverkill/Assets/AndrewFitzpatrick/Scripts/DonutAI.cs
@@ -10,10 +10,11 @@ public class DonutAI : MonoBehaviour
     public float rollSpeed;
     public float turnSpeed;
     public float attackRange;
-    public float deployTime;
+    public List<float> deployStages;
     private bool deployed;
     public float donutCircumference;
     private Transform modelTransform;
+    private float deployTimer = 0;
 
     // debug
     public GameObject target;
@@ -30,11 +31,19 @@ public class DonutAI : MonoBehaviour
         // deployTime
         findCircumference();
         modelTransform = GetComponentsInChildren<Transform>()[1];
+        changeColor(Color.white);
     }
 
     void Update()
     {
-        roll();
+        if(deployed)
+        {
+            deploySequence();
+        }
+        else
+        {
+            roll();
+        }
     }
 
     void roll()
@@ -51,6 +60,13 @@ public class DonutAI : MonoBehaviour
 
         // roll forward
         transform.Translate(transform.forward * rollSpeed * Time.deltaTime, Space.World);
+
+        // look for player
+        // deploy if within attackRange
+        if((transform.position - target.transform.position).magnitude < Mathf.Pow(attackRange, 2))
+        {
+            deployed = true;
+        }
     }
 
     // sets donutCircumference
@@ -68,5 +84,49 @@ public class DonutAI : MonoBehaviour
         // circumference is 2PIr aka PI * diameter
         // also takes into account scaling
         donutCircumference = (size * Mathf.PI * transform.localScale.y);
+    }
+
+    // fall over and attack player
+    void deploySequence()
+    {
+        // increase deployTimer
+        deployTimer += Time.deltaTime;
+
+        if (deployTimer < deployStages[0]) // number of seconds of fall over animation
+        {
+            // play fall over animation
+            changeColor(Color.red);
+            Debug.Log("falling over");
+        }
+        else if(deployTimer < deployStages[1])  // deployed
+        {
+            // shoot at player
+            changeColor(Color.green);
+            Debug.Log("deployed and shooting");
+        }
+        else if(deployTimer < deployStages[2]) // number of frames of get up animation
+        {
+            // play get up animation
+            changeColor(Color.blue);
+            Debug.Log("getting up");
+        }
+        else
+        {
+            // no longer deployed
+            // resume movement
+            deployed = false;
+            deployTimer = 0;
+            changeColor(Color.white);
+            Debug.Log("moving to player");
+        }
+    }
+
+    void changeColor(Color newColor)
+    {
+        MeshRenderer[] childMeshes = GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer currentMesh in childMeshes)
+        {
+            currentMesh.material.SetColor("_Color", newColor);
+        }
     }
 }
