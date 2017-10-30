d22c9428e9755fe1e4ec18ee01731553340dc473
Zackary D <zackary.direen@hotmail.com>
2017-10-30 11:29:44 +1100

    Pickup interactions.

---
+++ HumorousOverkill/Assets/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+
+    public int maxHealth;
+    private int currentHealth;
+
+    // Use this for initialization
+	void Start () {
+
+        currentHealth = maxHealth;
+	}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+
+    //}
+
+   public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+    }
+
+   public void HealDamage(int healAmount)
+    {
+        currentHealth += healAmount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
