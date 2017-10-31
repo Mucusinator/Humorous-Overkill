7fc6d7857fa4505c074e2f59b1fb444b7ac41982
blastsilver <blastsilver@gmail.com>
2017-10-31 22:23:12 +1100

    finished ui for now

---
+++ HumorousOverkill/Assets/Scripts/FranciscoRomano/UI/UIAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using EventHandler;
+using System.Collections;
+using System.Collections.Generic;
+
+[BindListener("uimanager", typeof(UIManager))]
+public class UIAction : EventHandle
+{
+    public GameEvent type;
+
+    public void SendEvent()
+    {
+        GetEventListener("uimanager").HandleEvent(type);
+    }
+}
