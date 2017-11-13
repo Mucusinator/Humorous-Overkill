98f9e0bde1525fa1698ed804a02d430319f15407
Zackary D <zackary.direen@hotmail.com>
2017-11-13 15:10:18 +1100

    ui images
    lit

---
+++ HumorousOverkill/Assets/imageChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class imageChange : MonoBehaviour {
+    // Two images, one for confetti, one for laser, one for the base.
+    public Sprite confettiImage, LaserImage;
+        
+    public Image baseImage;
+    // THe combined weapon script.
+    public CombinedScript weaponScript;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (weaponScript.gunType == CombinedScript.GunType.RIFLE)
+        {
+            baseImage.sprite = LaserImage;
+        }
+        else
+        {
+            baseImage.sprite = confettiImage;
+        }
+	}
+}
