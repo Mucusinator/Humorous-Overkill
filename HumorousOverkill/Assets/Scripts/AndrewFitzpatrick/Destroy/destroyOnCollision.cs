54e8f4d6f73fb4c572a13278643d30b417b4f38c
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-11-28 16:03:39 +1100

    disregard that now it's fixed

--- HumorousOverkill/Assets/Scripts/AndrewFitzpatrick/Destroy/destroyOnCollision.cs
+++ HumorousOverkill/Assets/Scripts/AndrewFitzpatrick/Destroy/destroyOnCollision.cs
@@ -7,7 +7,7 @@ public class destroyOnCollision : MonoBehaviour
     // destroys a gameobject when it collides with anything
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag != "Enemy" && other.gameObject.tag != "Manager")
+        if(other.gameObject.tag != "Enemy" && other.gameObject.tag != "Manager" && other.gameObject.GetComponent<Player>() != null)
         {
             Debug.Log(gameObject.name + " was destroyed when it collided with " + other.gameObject.name);
             Destroy(gameObject);
