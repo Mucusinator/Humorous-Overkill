f1bc9a8a2b0362b8c5145f82848796cdc0b43db8
Zackary D <zackary.direen@hotmail.com>
2017-11-28 11:39:24 +1100

    stuff

--- HumorousOverkill/Assets/Scripts/ZacDireen/CombinedScript.cs
+++ HumorousOverkill/Assets/Scripts/ZacDireen/CombinedScript.cs
@@ -178,8 +178,8 @@ public class CombinedScript : MonoBehaviour
         if (e.arg == GameEvent._NULL_)
             if (e.type == typeof(AudioManager)) {
                 m_audioManager = (AudioManager)s;
-                m_audioManager.Add(LazerSound);
-                m_audioManager.Add(shotgunSound);
+                m_audioManager.AddSound(LazerSound);
+                m_audioManager.AddSound(shotgunSound);
             }
         switch (e.arg)
         {
@@ -258,7 +258,7 @@ public class CombinedScript : MonoBehaviour
             shootShotgun();
         }
         if (Input.GetKeyDown(KeyCode.Mouse0) && gunType == GunType.RIFLE && currentRifleAmmo > 0) {
-            m_audioManager.Play(LazerSound);
+            m_audioManager.PlaySound(LazerSound,true);
         }
 
         if (Input.GetKeyUp(KeyCode.Mouse0))
