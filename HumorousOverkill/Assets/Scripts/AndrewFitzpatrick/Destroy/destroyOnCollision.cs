7daa283718221d0775fe9f00e6e7856d344d24bd
andrewthenintendrone <andrewfitzpatrick@y7mail.com>
2017-11-28 16:01:48 +1100

    fixed cupcake projectiles and removed bad grammar caused by tiredness

--- HumorousOverkill/Assets/Scripts/AndrewFitzpatrick/Destroy/destroyOnCollision.cs
+++ HumorousOverkill/Assets/Scripts/AndrewFitzpatrick/Destroy/destroyOnCollision.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class destroyOnCollision : MonoBehaviour
 {
     // destroys a gameobject when it collides with anything
-    void OnCollisionEnter(Collision other)
+    void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag != "Enemy" && other.gameObject.tag != "Manager")
         {
