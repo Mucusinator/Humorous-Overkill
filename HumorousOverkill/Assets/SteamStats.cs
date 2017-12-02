e063ac8f7f768751ed77ac0fa7a1866934ebc391
zackslay3r <zackary.direen@hotmail.com>
2017-12-02 20:32:29 +1100

    Using steam stuffz.

---
+++ HumorousOverkill/Assets/SteamStats.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facepunch.Steamworks;
+using UnityEngine.UI;
+
+public class SteamStats : MonoBehaviour {
+
+    private bool show = false;
+    public Button SteamButton;
+    public Text statText;
+
+    void Start()
+    {
+        Button btn = SteamButton.GetComponent<Button>();
+        btn.onClick.AddListener(ShowStats);
+    }
+
+    void ShowStats()
+    {
+        if (!show)
+        {
+            show = true;
+            
+            Display();
+        }
+        else
+        {
+            show = false;
+
+            Display();
+        }
+    }
+
+
+
+    void Display()
+    {
+        if (show)
+        {
+            statText.text = Client.Instance.Username;
+            Debug.Log(Client.Instance.Username);
+           
+        }
+        else
+        {
+            statText.text = " ";
+        }
+    }
+}
